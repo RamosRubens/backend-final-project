0bb3519f5f922fd7f33fcee7631b99ef17488edb
Rubens Ramos <rubensframos00@gmail.com>
2023-03-27 21:42:46 -0300

    Adicionando o método POST de resource

--- src/api/resources/routes.js
+++ src/api/resources/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const { any } = require('joi')
 const Joi = require('joi')
 const { NotFoundError, ValidationError } = require('../../errors')
 
@@ -55,31 +56,65 @@ const getResource = async(req, res) => {
 }
 
 const postResource = async(req, res) => {
+  try {
 
-  const listType = req.body.listType
+    const listType = req.body.listType
 
-  validatePost(listType)
+    validatePost(listType)
 
-  switch (listType) {
-    case ParamTypes.JOURNEYS:
-      journeyRepository
-        .list()
-        .then(journeys => res.status(200).send({journeys}))
-      break;
+    switch (listType) {
+      case ParamTypes.JOURNEYS:
+        journeyRepository
+          .list()
+          .then(journeys => res.status(200).send({journeys}))
+        break;
 
-    case ParamTypes.COURSES:
-      coursesRepository
-        .listCourses()
-        .then(courses => res.status(200).send({courses}))
+      case ParamTypes.COURSES:
+        coursesRepository
+          .listCourses()
+          .then(courses => res.status(200).send({courses}))
 
-      break;
-    case ParamTypes.LESSONS:
-      lessonsRepository
-        .list()
-        .then(lessons => res.status(200).send({lessons}))
-      break;
+        break;
+
+      case ParamTypes.LESSONS:
+        lessonsRepository
+          .list()
+          .then(lessons => res.status(200).send({lessons}))
+        break;
+
+      case ParamTypes.ALL:
+
+        const journeys = await journeyRepository.list()
+        const allJourneys = [];
+
+        for (const journey of journeys) {
+          const courses = await coursesRepository.getAllCoursesInAJorney(journey.id);
+          for (const course of courses) {
+            const lessons = await coursesRepository.getLessonsInACourse(course.id);
+            course.lessons = lessons
+          }
+
+          const journeyWithAllCourses = prepareCompleteObject(journey, courses);
+
+          allJourneys.push(journeyWithAllCourses);
+        }
+
+        res.status(200).send(allJourneys)
+        break;
+      }
+  } catch (error) {
+    res.status(400).send({error: error.message})
   }
 }
+
+const prepareCompleteObject = (journey, courses) => ({
+  id: journey.id,
+  name: journey.name,
+  courseDescription: journey.description,
+  thumb: journey.thumb,
+  slug: journey.slug,
+  courses: courses
+})
 
 const validateRequest = (resourceName, id) => {
 
